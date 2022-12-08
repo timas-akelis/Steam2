ad0ddce0db6dbee6e24542b1f0e8e518878d52f5
timas-akelis <84728870+timas-akelis@users.noreply.github.com>
2022-12-08 17:26:27 +0200

    Add to wishlist works

--- Steam2/Controllers/GamesController.cs
+++ Steam2/Controllers/GamesController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +46,7 @@ namespace Steam2.Controllers
         }
 
         // GET: Games/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -54,18 +57,20 @@ namespace Steam2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("Id,Title,PublishingDate,Price,Rating,Description,Developer,Publisher,SaleId")] Game game)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(game);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(game);
+            game.Id = CreateId();
+            game.SaleId = "None";
+
+            _context.Add(game);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index)); // ??
+            
         }
 
         // GET: Games/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(string id)
         {
             if (id == null || _context.Game == null)
@@ -86,6 +91,7 @@ namespace Steam2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Edit(string id, [Bind("Id,Title,PublishingDate,Price,Rating,Description,Developer,Publisher,SaleId")] Game game)
         {
             if (id != game.Id)
@@ -117,6 +123,7 @@ namespace Steam2.Controllers
         }
 
         // GET: Games/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(string id)
         {
             if (id == null || _context.Game == null)
@@ -137,6 +144,7 @@ namespace Steam2.Controllers
         // POST: Games/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
             if (_context.Game == null)
@@ -153,9 +161,60 @@ namespace Steam2.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        // POST: Games/AddWishlist/5
+        [Authorize]
+        public async Task<IActionResult> AddWishlist(string id)
+        {
+            //if (id == null || _context.Game == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //var game = await _context.Game.FindAsync(id);
+            //if (game == null)
+            //{
+            //    return NotFound();
+            //}
+            //return NotFound();
+            string ProfileId = GetId();
+            return RedirectToAction("Create", "Wishlists", new { GameId = id });
+        }
+
         private bool GameExists(string id)
         {
           return _context.Game.Any(e => e.Id == id);
+        }
+
+        private string CreateId()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[32];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+
+        private string GetId()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims
+                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null)
+                {
+                    return userIdClaim.Value;
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
--- Steam2/Controllers/WishlistsController.cs
+++ Steam2/Controllers/WishlistsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -22,7 +23,17 @@ namespace Steam2.Controllers
         // GET: Wishlists
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Wishlist.ToListAsync());
+            var wish = _context.Wishlist.ToList();
+            List<Game> allGames = new List<Game>();
+            for(int i = 0; i < wish.Count; i++)
+            {
+                var games = _context.Game.Where(x => x.Id == wish[i].GamesID).ToList();
+                for (int j = 0; j < games.Count; j++)
+                {
+                    allGames.Add(games[j]);
+                }
+            }
+            return View(allGames);
         }
 
         // GET: Wishlists/Details/5
@@ -43,26 +54,32 @@ namespace Steam2.Controllers
             return View(wishlist);
         }
 
-        // GET: Wishlists/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Wishlists/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Wishlists/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,GamesID,ProfileID,Date")] Wishlist wishlist)
+        public async Task<IActionResult> Create(string GameId)
         {
-            if (ModelState.IsValid)
+            Wishlist wishlist = new Wishlist();
+            wishlist.Id = CreateId();
+            wishlist.ProfileID = GetId();
+            wishlist.GamesID = GameId;
+            wishlist.Date = DateTime.Now;
+
+            var dup = _context.Wishlist.Where(m => m.GamesID == GameId).Any();
+            if (!dup)
             {
                 _context.Add(wishlist);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(wishlist);
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Wishlists/Edit/5
@@ -113,25 +130,28 @@ namespace Steam2.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(wishlist);
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Wishlists/Delete/5
-        public async Task<IActionResult> Delete(string id)
+        public async Task<IActionResult> Delete(string Id)
         {
-            if (id == null || _context.Wishlist == null)
+            if (Id == null || _context.Wishlist == null)
             {
                 return NotFound();
             }
 
             var wishlist = await _context.Wishlist
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .FirstOrDefaultAsync(m => m.GamesID == Id);
             if (wishlist == null)
             {
                 return NotFound();
             }
 
-            return View(wishlist);
+            _context.Wishlist.Remove(wishlist);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: Wishlists/Delete/5
@@ -156,6 +176,38 @@ namespace Steam2.Controllers
         private bool WishlistExists(string id)
         {
           return _context.Wishlist.Any(e => e.Id == id);
+        }
+
+        private string CreateId()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[32];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+
+        private string GetId()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userIdClaim = claimsIdentity.Claims
+                    .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null)
+                {
+                    return userIdClaim.Value;
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
