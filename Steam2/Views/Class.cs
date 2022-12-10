bdc8a0b5d9b998209fc9d5bbe1004d2916ab1f8a
Rokas666666 <60842961+Rokas666666@users.noreply.github.com>
2022-12-10 14:46:31 +0200

    tryign again

---
+++ Steam2/Views/Class.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Steam2.Views
+{
+    public static class MyExtensions
+    {
+        public static string GenerateRandomString(this HtmlHelper htmlHelper)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, 32)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
