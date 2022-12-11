0fe17096d101dc43e1d1d30de4e53813c85cb0cf
Rokas666666 <60842961+Rokas666666@users.noreply.github.com>
2022-12-11 10:14:23 +0200

    actually the vm

---
+++ Steam2/Models/ViewModels/GamesGenresAchievementsComments.cs
@@ -0,0 +1,18 @@
+﻿namespace Steam2.Models.ViewModels
+{
+    public class GameGenreAchievementComment
+    {
+        public Game Game { get; set; }
+        public List<string> ListedGenres { get; set; }
+        public Achievement Achievement { get; set; }
+        public Comment Comment { get; set; }
+
+        public GameGenreAchievementComment(Game InputGame, List<string> InputListedGenres, Achievement InputAchievement, Comment InputComment)
+        {
+            Game = InputGame;
+            ListedGenres = InputListedGenres;
+            Achievement = InputAchievement;
+            Comment = InputComment;
+        }
+    }
+}
